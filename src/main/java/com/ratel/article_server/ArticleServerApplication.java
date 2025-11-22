1bdd187b99b93e4ee1a20d3be89fcf37066ba248
Francis Kangaty <franciskangaty@gmail.com>
2025-11-22 20:52:48 +0300

    test commit terminal

---
+++ src/main/java/com/ratel/article_server/ArticleServerApplication.java
@@ -0,0 +1,13 @@
+package com.ratel.article_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratel/article_server/ArticleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratel.article_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
